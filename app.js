39556ba719b504a9191d4c5dda8c9a85d6d18e7f
igorrybalko <igorrybalko2009@gmail.com>
2020-12-12 20:22:02 +0200

    create lesson 2

--- app.js
+++ app.js
@@ -2,8 +2,27 @@ const express = require('express'),
 	app = express(),
 	port = 3000
 
-app.get('/', (req, res) => {
-	res.send('Hello world')
+app.get('/catalog/:id/section/:part', (req, res) => {
+	let info = 'catalog: ' + req.params.id + ' ' + 'part: ' + req.params.part;
+	res.send(info)
+})
+ 
+app.post('/', (req, res) => {
+	res.send('POST method...')
+})
+
+app.delete('/', (req, res) => {
+	res.send('DELETE method...')
+})
+
+app.route('/user').get((req, res) => {
+	res.send('get user...')
+}).post((req, res) => {
+	res.send('create user...')
+}).put((req, res) => {
+	res.send('update user...')
+}).delete((req, res) => {
+	res.send('delete user...')
 })
 
 app.listen(port, () => {
